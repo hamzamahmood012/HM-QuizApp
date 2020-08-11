ce13b304dbf1461fe93bf865e9b55b500fb70e91
hamzamahmood012 <hamzamahmood012@gmail.com>
2020-08-11 18:27:29 +0500

    HM-Calculator

---
+++ js/question.js
@@ -0,0 +1,62 @@
+
+
+
+// Arrray of object
+const quiz = [
+    {
+        q: ' The first page of a website is called?',
+        options:['Design page','Home page','First page','Main page'],
+        answer:1
+    },
+    {
+    q: 'Head Tag is Used for?',
+    options:[' Writing Styles','Writing Java Scripts','Including CSS,JS Files','All of above'],
+    answer:3
+},
+{
+    q:  "Which is the largest Heading Tag?",
+    options:['H1','H2',' H3','H4'],
+    answer:0
+},
+{
+    q: ' What is the Full Form of HTML?',
+    options:['Hyper Text Markup Language','Hyper Text Marking Language',' Hyper Text Machine Language','High Text Markup Language'],
+    answer:0
+},
+{
+    q:'What is the full form of HTTP?',
+    options:['Hyper Text Transfer Protocol','Hyper Text Transfer Package','Hyper Text Test Program','None of the above'],
+    answer:0
+}
+,
+{
+    q:' What is the search engine?',
+    options:['a program that search engines','a website that search anything','a hardware components','a machinery engine that search data'],
+    answer:1
+}
+,
+{
+    q:'Which tag is used to create body text in HTML?',
+    options:['HTML','Text','Title','Body'],
+    answer:3
+}
+
+,
+{
+    q:' Which extension is used to save the HTML file?',
+    options:['Compiler','Interpreter','Web Browser','Software'],
+    answer:2
+}
+,
+{
+    q:'Which of the following is used to view web pages created in HTML?',
+    options:['.docx','.doc','.xml','.html'],
+    answer:3
+}
+,
+{
+    q:'  Which links to the website in other home pages and other pages on the same site?',
+    options:['Hyper Link','Jam Link','Internet Protocol','HTTP'],
+    answer:0
+}
+]
